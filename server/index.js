a46c54714c776c1fdbf7b3d6e1554fd3fc597240
Amal Sony <amalsony5@gmail.com>
2023-08-27 18:19:54 -0400

    added image upload

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const axios = require("axios");
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const connectDB = require("./config/db");
 const port = process.env.PORT || 8000;
+
+require("dotenv").config();
 
 // Connect to MongoDB
 connectDB();
@@ -15,31 +19,92 @@ const mongoose = require("mongoose");
 // Middleware
 app.use(express.json());
 
+// Image upload imports
+const { uploadFile, deleteFile, getObjectSignedUrl } = require("./aws/s3");
+const multer = require("multer");
+const sharp = require("sharp");
+const crypto = require("crypto");
+
+// file upload
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+const generateFileName = (bytes = 32) =>
+  crypto.randomBytes(bytes).toString("hex");
+
+// validators
+const { validateGarmentInput } = require("./utilities/validators");
+
 // Routes
 
 // Add garment
-app.post("/api/add-garment", async (req, res) => {
-  try {
-    // Create a new garment and add the date and time to createdAt
-    const garment = new Garment({
-      ...req.body,
-      createdAt: new Date().toISOString(),
-    });
+app.post(
+  "/api/add-garment",
+  upload.single("garment-image"),
+  async (req, res) => {
+    const file = req.file;
 
-    // Save the garment to the database
-    await garment.save();
+    if (!file) {
+      return res.status(400).send({
+        errors: {
+          message: "Please add a garment image",
+        },
+      });
+    }
 
-    res.status(201).json({
-      success: true,
-      data: garment,
-    });
-  } catch (err) {
-    res.status(400).json({
-      success: false,
-      error: err,
-    });
+    const { type, name, brand, colors } = req.body;
+
+    const { valid, errors } = validateGarmentInput(type, name, brand, colors);
+
+    if (!valid) {
+      return res.status(400).send({
+        errors,
+      });
+    }
+
+    try {
+      // image name
+      const imageName = generateFileName();
+
+      if (!file.buffer) {
+        return res.status(400).send({
+          errors: {
+            message: "Please add a garment image",
+          },
+        });
+      }
+
+      // create a jpeg image
+      const fileBuffer = await sharp(file.buffer)
+        .resize(400, 400)
+        .jpeg({ quality: 80 })
+        .toBuffer();
+
+      await uploadFile(fileBuffer, imageName, file.mimetype);
+
+      // Create a new garment and add the date and time to createdAt
+      const garment = new Garment({
+        ...req.body,
+        createdAt: new Date().toISOString(),
+        image_url: imageName,
+      });
+
+      // Save the garment to the database
+      await garment.save();
+
+      res.status(201).json({
+        success: true,
+        data: garment,
+      });
+    } catch (err) {
+      res.status(400).json({
+        success: false,
+        error: err,
+      });
+
+      console.log(err);
+    }
   }
-});
+);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
---
+++ server/utilities/validators.js
@@ -0,0 +1,24 @@
+module.exports.validateGarmentInput = (type, name, brand, colors) => {
+  const errors = {};
+
+  if (!type || type.trim() === "") {
+    errors.type = "Type must not be empty";
+  }
+
+  if (!name || name.trim() === "") {
+    errors.name = "Name must not be empty";
+  }
+
+  if (!brand || brand.trim() === "") {
+    errors.brand = "Brand must not be empty";
+  }
+
+  if (!colors || colors.length === 0) {
+    errors.colors = "Colors must not be empty";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+};
