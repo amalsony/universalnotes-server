b324127017e8ab288ebf959638f8a4b1e98443c0
Amal Sony <amalsony5@gmail.com>
2023-10-19 17:04:06 -0400

    price no currency test

--- server/config/testData.js
+++ server/config/testData.js
@@ -5,7 +5,7 @@ module.exports.testData = [
     brand: "JCrew",
     image:
       "https://www.jcrew.com/s7-img-facade/J8214_GR5818_m?hei=975&crop=0,0,780,0",
-    price: 50,
+    price: "$50",
     link: "https://factory.jcrew.com/p/mens/categories/clothing/tees/henley-tees/long-sleeve-henley-in-slub-cotton/J8214?color_name=dark-eucalyptus&N=MEDIUM&noPopUp=true&srccode=Paid_Search%7cSearch_Pmax%7cGoogle%7cPX_ACQ_XPROD_SHIRTSXXXXXXX_EVG_ROAS_XXX_COUSA_EN_EN_A_FACT_GO_SH_PMX_xxxxxxxxxx,,,71700000112752265,,&utm_source=google&utm_medium=paid_search&utm_campaign=PX_ACQ_XPROD_SHIRTSXXXXXXX_EVG_ROAS_XXX_COUSA_EN_EN_A_FACT_GO_SH_PMX_xxxxxxxxxx,&utm_content=shopping&NoPopUp=True&gclid=CjwKCAjwp8OpBhAFEiwAG7NaEm8MEouF3lxrKMDokpzo2UyyXEuclZD4HAOXxwd_y05ySER88N_WDBoCNQoQAvD_BwE&gclsrc=aw.ds",
   },
   {
@@ -14,7 +14,7 @@ module.exports.testData = [
     brand: "Zara",
     image:
       "https://www.kosha.co/journal/wp-content/uploads/2022/07/winter-jacket-for-men.jpeg",
-    price: 50,
+    price: "$29",
     link: "https://www.zara.com/us/en/oversized-sweater-p06893308.html?v1=89170479&v2=1718129",
   },
   {
@@ -22,7 +22,7 @@ module.exports.testData = [
     name: "Sweater",
     brand: "Zara",
     image: "https://www.gap.com/webcontent/0053/682/159/cn53682159.jpg",
-    price: 50,
+    price: "$42",
     link: "https://www.zara.com/us/en/oversized-sweater-p06893308.html?v1=89170479&v2=1718129",
   },
 ];
