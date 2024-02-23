39e770791383fe147fbb9fa3db8f9bc5957900a4
Amal Sony <amalsony5@gmail.com>
2024-02-23 01:52:03 -0500

    accessCode 6 character mandate

--- server/utilities/createAccessCode.js
+++ server/utilities/createAccessCode.js
@@ -2,7 +2,7 @@ const AccessCode = require("../models/AccessCode");
 
 async function createAccessCode(referrer, isSingleUse) {
   console.log("function createAccessCode called");
-  const accessCode = Math.random().toString(36).substring(7).toUpperCase();
+  const accessCode = Math.random().toString(36).substring(2, 8).toUpperCase();
   const newAccessCode = new AccessCode({
     referrer,
     isSingleUse,
