22751e395d096dfe492a3fdbbfc78007f1872ab5
Amal Sony <amalsony5@gmail.com>
2024-06-22 01:05:46 -0400

    add x and facebook to blocked pages

--- server/utilities/blockedPages.js
+++ server/utilities/blockedPages.js
@@ -2,6 +2,9 @@ const blockedPages = [
   { domain: "instagram.com", path: "/" },
   { domain: "twitter.com", path: "/" },
   { domain: "twitter.com", path: "/home" },
+  { domain: "x.com", path: "/" },
+  { domain: "x.com", path: "/home" },
+  { domain: "facebook.com", path: "/" },
   { domain: "tiktok.com", path: "/" },
 ];
 
